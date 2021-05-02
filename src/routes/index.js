6ff43675209b831a856ddc3d7d9d31b21e052e1f
EmIT <emad.armoun@gmail.com>
2021-05-02 19:32:40 +0430

    Read environment data from .env file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
 
 /* GET home page. */
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Xanbilak 1st API' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: ('Welcome to Xanbilak 1st API, ' + testEnvironmentVariable) }));
 
 export default indexRouter;
