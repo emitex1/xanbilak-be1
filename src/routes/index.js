48d34b0509f7ca4388cfdad568cd35165f608f54
EmIT <emad.armoun@gmail.com>
2021-05-03 10:11:51 +0430

    Develop initial code for connection to DB

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { testEnvironmentVariable } from '../settings';
+import { messagesPage } from './messages';
 
 const indexRouter = express.Router();
 
 /* GET home page. */
 indexRouter.get('/', (req, res) => res.status(200).json({ message: ('Welcome to Xanbilak 1st API, ' + testEnvironmentVariable) }));
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
--- src/settings.js
+++ src/settings.js
@@ -9,3 +9,4 @@ dotenv.config();
 // console.log(result.parsed);
 
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
+export const connectionString = process.env.CONNECTION_STRING;
