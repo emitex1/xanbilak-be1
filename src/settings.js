e80c8715223ea6455e70c785241650241fa7bbf8
EmIT <emad.armoun@gmail.com>
2021-05-03 10:23:39 +0430

    Fix ESLint warnings

--- src/settings.js
+++ src/settings.js
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 // const result = dotenv.config();
- 
+
 // if (result.error) {
 //   throw result.error
 // }
- 
+
 // console.log(result.parsed);
 
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
