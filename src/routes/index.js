7118578a8b75f98d7fc10de5bb4d52da740bf6a3
EmIT <emad.armoun@gmail.com>
2021-05-04 22:31:38 +0430

    Add middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { testEnvironmentVariable } from '../settings';
 import { messagesPage } from './messages';
+import { modifyMessage } from '../middleware';
+import { addMessage } from '../controllers/messages';
 
 const indexRouter = express.Router();
 
 /* GET home page. */
-indexRouter.get('/', (req, res) => res.status(200).json({ message: ('Welcome to Xanbilak 1st API, ' + testEnvironmentVariable) }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: (`Welcome to Xanbilak 1st API, ${testEnvironmentVariable}`) }));
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
