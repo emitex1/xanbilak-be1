363538968c38d72c929ee082d7f3770b4c2e1b49
EmIT <emad.armoun@gmail.com>
2021-05-05 08:07:25 +0430

    Add Error HAndler & 404 Error

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-// var createError = require('http-errors');
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import createError from 'http-errors';
 
 import indexRouter from './routes/index';
 
@@ -15,19 +15,19 @@ app.use(cookieParser());
 app.use('/v1', indexRouter);
 
 // catch 404 and forward to error handler
-/* app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
-}); */
+});
 
 // error handler
-/* app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  res.status(err.status || 400).json({
+    message: err && err.message ? err.message : 'Unknown Error',
+    error: req.app.get('env') === 'development' ? err.stack : {}
+  });
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-}); */
+  // res.render('error');
+});
 
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { testEnvironmentVariable } from '../settings';
 import { messagesPage } from './messages';
-import { modifyMessage } from '../middleware';
+import { modifyMessage, performAsyncAction } from '../middleware';
 import { addMessage } from '../controllers/messages';
 
 const indexRouter = express.Router();
@@ -9,6 +9,6 @@ const indexRouter = express.Router();
 /* GET home page. */
 indexRouter.get('/', (req, res) => res.status(200).json({ message: (`Welcome to Xanbilak 1st API, ${testEnvironmentVariable}`) }));
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', modifyMessage, addMessage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 export default indexRouter;
