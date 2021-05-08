18e29b2909927aaec3a7cfe0bc58edcb4c8752f3
EmIT <emad.armoun@gmail.com>
2021-05-08 18:30:23 +0430

    Update types

--- src/utils/queryFunctions.ts
+++ src/utils/queryFunctions.ts
@@ -6,8 +6,8 @@ import {
 } from './queries';
 
 export const executeQueryArray = async (arr: Array<string>) => new Promise<void>(resolve => {
-  const stop = arr.length;
-  arr.forEach(async (q, index) => {
+  const stop: number = arr.length;
+  arr.forEach(async (q: string, index: number) => {
     await pool.query(q);
     if (index + 1 === stop) resolve();
   });
