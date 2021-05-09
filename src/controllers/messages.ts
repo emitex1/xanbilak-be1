58fa12b4efc9b2e50d2fe03651646b91507053b0
EmIT <emad.armoun@gmail.com>
2021-05-09 06:13:21 +0430

    add new line

--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -1,6 +1,7 @@
 import Model from '../models/model';
 
 const messagesModel: any = new Model('messages');
+
 export const messagesPage = async (req: any, res: any) => {
   try {
     const data: any = await messagesModel.select('name, message');
