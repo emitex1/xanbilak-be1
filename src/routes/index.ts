4808246281bcd15043c4690b175069479718282e
EmIT <emad.armoun@gmail.com>
2021-05-10 06:25:08 +0430

    Make the code more clean

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { testEnvironmentVariable } from '../settings';
-import { messagesPage } from './messages';
+
+/** Middlewares */
 import { modifyMessage, performAsyncAction } from '../middleware';
-import { addMessage } from '../controllers/messages';
-import { readShoppingItems } from './shoppingItems';
+
+/** Controllers */
+import { messagesPage, addMessage } from '../controllers/messages';
+import { readShoppingItems } from '../controllers/shoppingItems';
 
 const indexRouter = express.Router();
 
-/* GET home page. */
 indexRouter.get('/', (req, res) => res.status(200).json({ message: (`Welcome to Xanbilak 1st API, Message : ${testEnvironmentVariable}`) }));
 indexRouter.get('/messages', messagesPage);
 indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
