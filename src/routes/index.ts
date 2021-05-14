36425c435337c0e9c6ee2f2a1df7d86882061ed2
EmIT <emad.armoun@gmail.com>
2021-05-14 17:56:14 +0430

    Complete ShoppingItem getter API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,17 +2,14 @@ import express from 'express';
 import { testEnvironmentVariable } from '../settings';
 
 /** Middlewares */
-import { modifyMessage, performAsyncAction } from '../middleware';
+//import { modifyMessage, performAsyncAction } from '../middleware';
 
 /** Controllers */
-import { messagesPage, addMessage } from '../controllers/messages';
-import { readShoppingItems } from '../controllers/shoppingItems';
+import { readShoppingListItems } from '../controllers/shoppingItems';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) => res.status(200).json({ message: (`Welcome to Xanbilak 1st API, Message : ${testEnvironmentVariable}`) }));
-indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
-indexRouter.get('/sic', readShoppingItems);
+indexRouter.get('/shoppingList', readShoppingListItems);
 
 export default indexRouter;
