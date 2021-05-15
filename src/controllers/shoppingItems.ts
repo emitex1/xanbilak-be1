8bd3c8d94ed03ad763d444360932b897065a86a6
EmIT <emad.armoun@gmail.com>
2021-05-15 07:39:27 +0430

    Upgrade ShoppingItem schema model

--- src/controllers/shoppingItems.ts
+++ src/controllers/shoppingItems.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 export const readShoppingListItems = async (req: any, res: any) => {
     const prisma = new PrismaClient();
-    const items = await prisma.shopping_items.findMany();
+    const items = await prisma.shoppingItems.findMany();
     res.status(200).json(items);
 }
